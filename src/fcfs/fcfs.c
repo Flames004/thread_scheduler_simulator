22718b426cc54fe617e7e4e35b4ba9ed96bdd098
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-30 12:25:09 +0530

    folder and file structure maintain

--- src/fcfs/fcfs.c
+++ src/fcfs/fcfs.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fcfs.h"
+
+typedef struct {
+    int pid, arrival, burst, start, completion, turnaround, waiting;
+} Process;
+
+void inputProcesses(Process p[], int n) {
+    for(int i=0; i<n; i++) {
+        p[i].pid = i+1;
+        printf("Enter Arrival Time and Burst Time for P%d: ", i+1);
+        scanf("%d %d", &p[i].arrival, &p[i].burst);
+    }
+}
 
 void fcfs(Process p[], int n) {
-    // Sort by arrival time
     for(int i=0; i<n-1; i++) {
         for(int j=i+1; j<n; j++) {
             if(p[i].arrival > p[j].arrival) {
@@ -17,8 +27,7 @@ void fcfs(Process p[], int n) {
     int time = 0;
     float totalWT = 0, totalTAT = 0;
 
-    printf("\n--- FCFS Scheduling ---\n");
-    printf("Gantt Chart:\n");
+    printf("\n--- FCFS Scheduling ---\nGantt Chart:\n");
 
     for(int i=0; i<n; i++) {
         if(time < p[i].arrival) time = p[i].arrival;
@@ -28,16 +37,26 @@ void fcfs(Process p[], int n) {
         p[i].waiting = p[i].turnaround - p[i].burst;
 
         time = p[i].completion;
-
         totalWT += p[i].waiting;
         totalTAT += p[i].turnaround;
 
         printf("| P%d ", p[i].pid);
     }
+
     printf("|\n0");
     for(int i=0; i<n; i++)
         printf("   %d", p[i].completion);
 
     printf("\n\nAverage Waiting Time: %.2f", totalWT/n);
     printf("\nAverage Turnaround Time: %.2f\n", totalTAT/n);
+}
+
+int main() {
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    Process p[n];
+    inputProcesses(p, n);
+    fcfs(p, n);
+    return 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fcfs.h"
-
-void inputProcesses(Process p[], int n) {
-    for(int i=0; i<n; i++) {
-        p[i].pid = i+1;
-        printf("Enter Arrival Time, Burst Time, Priority for P%d: ", i+1);
-        scanf("%d %d %d", &p[i].arrival, &p[i].burst, &p[i].priority);
-    }
-}
 
 int main() {
-    int n, choice;
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
+    int choice;
 
-    Process p[n];
-    inputProcesses(p, n);
+    while(1) {
+        printf("\n===== CPU Scheduling Simulator =====\n");
+        printf("1. First Come First Serve (FCFS)\n");
+        printf("2. Shortest Job First (SJF - Non Preemptive)\n");
+        printf("3. Round Robin\n");
+        printf("4. Priority Scheduling\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    printf("\nChoose Scheduling Algorithm:\n");
-    printf("1. FCFS\n2. SJF (Non-Preemptive)\n3. Round Robin\n4. Priority\n");
-    scanf("%d", &choice);
-
-    switch(choice) {
-        case 1: fcfs(p, n); break;
-        // other algorithms later
-        default: printf("Invalid choice!\n");
+        switch(choice) {
+            case 1:
+                system("./fcfs/fcfs");
+                break;
+            case 2:
+                system("./sjf/sjf");
+                break;
+            case 3:
+                system("./round_robin/rr");
+                break;
+            case 4:
+                system("./priority/priority");
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
     }
 
     return 0;
